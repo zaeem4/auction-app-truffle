cf50bb9b680d5777ad961426f5b54c19c53cc12b
zaeem4 <zaeem1169@gmail.com>
2023-12-19 13:34:46 +0500

    solidity auction app- coursera

---
+++ truffle-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  //npm install solc@0.4.17
+  compilers: {
+    solc: {
+      version: "^0.4.17",
+    },
+  },
+};
